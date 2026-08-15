f724ac409933474e4b890c0699020a16bbc52db2
bhcabrera1991 <cabrerabrunohenrique@gmail.com>
2026-08-15 07:24:14 -0300

    criado a funcao para permitir só alteracao com o login, e corrigido para acessar s´po logado

--- ordem_de_servico/sqlatualizaros.php
+++ ordem_de_servico/sqlatualizaros.php
@@ -1,11 +1,38 @@
 <?php
 
+
+    session_start();
+
+    if(!isset($_SESSION['id_usuario'])){
+        header('Location:../index.php');
+        exit;
+
+    }
+
     $codigo =$_POST['codigo_ordem_de_servico']??'';
     $codigodoproduto =$_POST['codigo_do_produto']??'';
     $nomedoprodutoos=mb_strtolower( $_POST['nome_do_produto']??'','utf-8');
     $quantidade =$_POST['quantidade_entrada']??'';
     $data = $_POST['data']??'';
     $responsavelb = $_POST['responsavelb'] ??'';
+
+
+
+    if($_SESSION['id_usuario'] != $responsavelb){
+        echo "<link rel ='stylesheet' href='../css/style.css'>";
+
+            echo "
+            <div class='alert-container'>
+                <div class='alert-box'>
+                    <h1> Erro. Alteração NÃO CONCLUIDA</h1>
+                    <h2 class='alert-title' >Atenção: Responsavel e logim são divirgente.</h2>
+                    <p> Faça o Login novamente</p>
+                    <a class='btn-back alert-text ' href='atualizar_os.php'>Voltar Pagina</a>
+                </div>
+            </div>";
+
+        exit;
+    }
 
     function os($codigo){
         $conn = mysqli_connect("localhost","root","","bdprojetosenac");
@@ -28,6 +55,32 @@
         }
     }
 
+    function codigo_nome_produto($codigodoproduto,$nomedoprodutoos){
+        $conn = mysqli_connect("localhost","root","","bdprojetosenac");
+        if(!$conn){
+            die("</h1>Erro_2</h1>".mysqli_connect_error());
+            exit;
+        }
+
+        $sql ="select codigoproduto, nomeProduto from tbcadastropeca where codigoproduto = '$codigodoproduto' and nomeProduto ='$nomedoprodutoos'";
+
+        $resultado1 = mysqli_query($conn,$sql);
+        
+        if(mysqli_num_rows($resultado1)>0){
+            mysqli_close($conn);
+            return true;
+        }
+        else{
+            mysqli_close($conn);
+            return false;
+
+        }
+
+
+    }
+
+    codigo_nome_produto($codigodoproduto,$nomedoprodutoos);
+
     if(empty($codigo)){
          die("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css'>;
             <link rel ='stylesheet' href='../css/style.css'>
@@ -46,9 +99,26 @@
 
     
     }
+    if (!os($codigo)){
+        echo "<link rel ='stylesheet' href='../css/style.css'>";
+
+            echo "
+            <div class='alert-container'>
+                <div class='alert-box'>
+                    <h1 class='alert-title' >Erro: Esta Ordem de Serviço não existe no sistema.</h1>
+                    
+                  
+                    <a class='btn-back alert-text ' href='atualizar_os.php'>Voltar Pagina</a>
+                </div>
+            </div>";
+
+        exit;
 
 
-    if(os($codigo)){
+
+    }
+
+    if(codigo_nome_produto($codigodoproduto,$nomedoprodutoos)){
         $conn = mysqli_connect("localhost", "root", "", "bdprojetosenac"); 
     
        $sql = "UPDATE tbordemservico SET data_alteracao ='$data', codigoProduto = '$codigodoproduto', nomeProduto = '$nomedoprodutoos', quantidadeProduzida = '$quantidade', responsavel_alteracao = '$responsavelb' WHERE codigoOS = '$codigo'"; 
@@ -56,27 +126,43 @@
         $r = mysqli_query($conn, $sql); 
     
         if($r){ 
-            mysqli_close($conn); 
-            echo 'Atualizado com Sucesso'; 
-             echo"<link rel ='stylesheet' href='css/style.css'> 
-                <div style='display: flex; justify-content: center;' >
-                    <div class=''>
-                        <a class='cp caixa  fontemenu' href='atualizar_os.php'>Voltar</a>
-                    </div>
-                </div>";
+            mysqli_close($conn);
+            echo "<link rel ='stylesheet' href='../css/style.css'>";
+
+            echo "
+            <div class='success-container'>
+                <div class='success-box'>
+                    <h1 class='success-title' >Atualizado com Sucesso</h1>
+                    
+                  
+                    <a class='btn-success success-text ' href='atualizar_os.php'>Voltar Pagina</a>
+                </div>
+            </div>";
+
+
+          
                 exit; 
         } 
         mysqli_close($conn);
 
-    } else {
-    echo "Erro: Esta Ordem de Serviço não existe no sistema.";
-    echo"<link rel ='stylesheet' href='css/style.css'> 
-    <div style='display: flex; justify-content: center;' >
-        <div class=''>
-            <a class='cp caixa  fontemenu' href='atualizar_os.php'>Voltar</a>
-        </div>
-    </div>";
+    }  else {
+
+     echo "<link rel ='stylesheet' href='../css/style.css'>";
+
+            echo "
+            <div class='alert-container'>
+                <div class='alert-box'>
+                    <h1 class='alert-title' >Erro: Codigo do Produto x Nome Produto.</h1>
+                    
+                  
+                    <a class='btn-back alert-text ' href='atualizar_os.php'>Voltar Pagina</a>
+                </div>
+            </div>";
+
+        exit;
     }
+
+
 
 
 
